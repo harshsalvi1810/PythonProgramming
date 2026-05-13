b2a7937f286334ff6240a41210b64b65f5dfd30b
harshsalvi1810 <harshsalvi1810@gmail.com>
2026-05-13 17:01:21 +0530

    First Commit Done

---
+++ Fundamentals/python.py
@@ -0,0 +1,58 @@
+'''
+multi line comment
+
+Q ASK USER TO ENTER TWO NUMBERSAND DO THE ADDITION OF TWO NUMBERS
+
+LOGIC :
+
+1. I WILL ASK USER TO ENTER TWO NUMBER
+2. TWO NUMBERS WILL BE ASSIGNED TO THEE TWO VARIABLES
+3. ONE VARIABLE : ANS = V1 + V2
+4.PRINT THE ANSWER
+
+EXECUTION:
+1. input() :  
+2.assign 2 variable with input
+3. assign variabkes ans : v1 + v2
+4.print output
+
+'''
+# %%
+
+#ask user for input
+
+v1 = int(input("Enter First Number: "))
+v2 = int(input("Enter Second Number: "))
+
+ans = v1 + v2
+
+print(ans)
+
+print(type(ans))
+
+name = input("Enter the name: ")
+
+print(type(name))
+
+#data types
+'''
+int
+float
+string
+boolean
+list
+tuple
+set
+dictionary
+''' 
+# _ = 100 
+# print(_)
+
+# name = 'name' need qotes for string
+
+# multivariable assignment     a,b,c = int,float,str 
+
+# boolean will take only camel case = True,False not true
+
+# * allowed for int and str
+
